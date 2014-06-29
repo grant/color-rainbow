061437d65410cd854d4f09151ff74b21771c2079
Grant Timmerman <granttimmerman@gmail.com>
2014-06-28 23:36:02 -0700

    Add more color implementation

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 
 // A rainbow generator
 
-var Rainbow = function (numberOfColors) {
+var Color = require('color');
+var MAX_RGB = 255;
+var NUM_SECTIONS = 6;
 
-  console.log('hi');
+// Gets the rainbow color given a normalized index (0-1)
+// Returns a color
+var getColor = function (index) {
+  var section = Math.floor(index/NUM_SECTIONS);
+  var start = (index - (section/NUM_SECTIONS))/NUM_SECTIONS; // index from start of section (0-1)
+  var end = 1 - start; // index from end of section (0-1)
+
+  var colorArray;// the rgb color array with colors that range from 0-1.
+  switch (section) {
+    case 0:
+      colorArray = [1, 0, start];
+      break;
+    case 1:
+      colorArray = [end, 0, 1];
+      break;
+    case 2:
+      colorArray = [0, start, 1];
+      break;
+    case 3:
+      colorArray = [0, 1, end];
+      break;
+    case 4:
+      colorArray = [start, 1, 0];
+      break;
+    case 5:
+      colorArray = [1, end, 0];
+      break;
+  }
+
+  // Return the color
+  return new Color().rgb(colorArray.map(function(value) {
+    return value * MAX_RGB;
+  }));
 };
 
-
-
+var Rainbow = function (numberOfColors) {
+  var rainbow = [];
+  for (var i = 0; i < numberOfColors; ++i) {
+    rainbow[i] = getColor(i/numberOfColors);
+  }
+  return rainbow;
+};
 
 module.exports = Rainbow;
--- test.js
+++ test.js
@@ -4,5 +4,7 @@
 describe('rainbow', function () {
   it('should work with the static implementation', function () {
     var Rainbow = require('./');
+
+    Rainbow(1)[0].rgbArray().should.equal([255, 0, 0]);
   });
 });
